7168228e0280171b5388fd5e11ed5649397fdec3
Emmie <98668340+emolucy@users.noreply.github.com>
2026-04-21 23:26:34 -0400

    silence warnings

--- home/home.nix
+++ home/home.nix
@@ -21,10 +21,13 @@
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
+    withRuby = false;
+    withPython3 = false;
   };
 
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
     matchBlocks."github.com" = {
       identityFile = "~/.ssh/id_ed25519";
     };
