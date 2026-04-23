274c45bce1cf1aed302a54163ab8721b197643a2
Emmie <98668340+emolucy@users.noreply.github.com>
2026-04-23 11:34:11 -0400

    oops lua comments are not the same as nix comments!

--- home/nvim/treesitter.lua
+++ home/nvim/treesitter.lua
@@ -1,4 +1,4 @@
-# set up highlighting and indent for ts
+-- set up highlighting and indent for ts
 vim.api.nvim_create_autocmd("FileType", {
     callback = function(args)
         pcall(vim.treesitter.start, args.buf)
