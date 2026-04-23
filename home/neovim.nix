f8df231b1daceafb7e8509335c63c2d13d171868
Emmie <98668340+emolucy@users.noreply.github.com>
2026-04-23 11:25:00 -0400

    add options for nvim

--- home/neovim.nix
+++ home/neovim.nix
@@ -6,9 +6,29 @@
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
+    withRuby = false;
+    withPython3 = false;
 
     plugins = with pkgs.vimPlugins; [
 
+    ];
+
+    # lsps and formatters
+    extraPackages = with pkgs; [
+      # nix
+      nil
+      nixfmt
+
+      # lua
+      lua-language-server
+      stylua
+    ];
+
+    extraLuaConfig = builtins.concatStringsSep "\n" [
+      (builtins.readFile ./nvim/options.lua)
+      (builtins.readFile ./nvim/keymaps.lua)
+      (builtins.readFile ./nvim/autocmds.lua)
+      (builtins.readFile ./nvim/treesitter.lua)
     ];
 
   };
