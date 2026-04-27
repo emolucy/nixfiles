4ee156410c394ca02903a15f9792a180ecc4ae19
Emmie <98668340+emolucy@users.noreply.github.com>
2026-04-27 11:15:05 -0400

    fix indenting in flake

--- flake.nix
+++ flake.nix
@@ -1,24 +1,29 @@
 {
-    description = "nixfiles by emmie paw.";
+  description = "nixfiles by emmie paw.";
 
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-        home-manager = {
-            url = "github:nix-community/home-manager";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-        catppuccin.url = "github:catppuccin/nix";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+    catppuccin.url = "github:catppuccin/nix";
+  };
 
-    outputs =
-        { nixpkgs, home-manager, catppuccin, ... }:
-        {
-            homeConfigurations.emmie = home-manager.lib.homeManagerConfiguration {
-                pkgs = nixpkgs.legacyPackages.x86_64-linux;
-                modules = [ 
-		  ./home/home.nix
-		  catppuccin.homeModules.catppuccin
-		];
-            };
-        };
+  outputs =
+    {
+      nixpkgs,
+      home-manager,
+      catppuccin,
+      ...
+    }:
+    {
+      homeConfigurations.emmie = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = [
+          ./home/home.nix
+          catppuccin.homeModules.catppuccin
+        ];
+      };
+    };
 }
