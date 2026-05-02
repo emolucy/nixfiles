0e845073ad86717cdbc12cc91ac5ee2cd4497f3d
Emmie <98668340+emolucy@users.noreply.github.com>
2026-05-02 13:31:25 -0400

    try this fix

--- home/nvim/plugin/luasnip-latex.lua
+++ home/nvim/plugin/luasnip-latex.lua
@@ -3,26 +3,27 @@ local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 
+vim.api.nvim_create_autocmd("FileType", {
+	group = "Autocmds",
+	pattern = "tex",
+	callback = function()
+		local ls = require("luasnip")
+		local existing = ls.get_snippets("tex")
+		if not existing then
+			return
+		end
+		for idx = #existing, 1, -1 do
+			local trig = existing[idx].trigger
+			if trig == "mk" or trig == "dm" then
+				table.remove(existing, idx)
+			end
+		end
+	end,
+})
+
 local function not_math()
 	return vim.fn["vimtex#syntax#in_mathzone"]() == 0
 end
-
-local function remove_by_trigger(filetype, triggers)
-	local existing = ls.get_snippets(filetype)
-	if not existing then
-		return
-	end
-	for idx = #existing, 1, -1 do
-		for _, trig in ipairs(triggers) do
-			if existing[idx].trigger == trig then
-				table.remove(existing, idx)
-				break
-			end
-		end
-	end
-end
-
-remove_by_trigger("tex", { "dm", "mk" })
 
 ls.add_snippets("tex", {
 	s({ trig = "mk", snippetType = "autosnippet", condition = not_math }, {
