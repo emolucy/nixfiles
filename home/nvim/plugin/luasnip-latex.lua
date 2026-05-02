8df16571c4707deeb4d51da5b154ccf18075e083
Emmie <98668340+emolucy@users.noreply.github.com>
2026-05-02 13:34:38 -0400

    try try again

--- home/nvim/plugin/luasnip-latex.lua
+++ home/nvim/plugin/luasnip-latex.lua
@@ -3,38 +3,41 @@ local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 
-vim.api.nvim_create_autocmd("FileType", {
-	group = "Autocmds",
-	pattern = "tex",
-	callback = function()
-		local ls = require("luasnip")
-		local existing = ls.get_snippets("tex")
-		if not existing then
-			return
-		end
-		for idx = #existing, 1, -1 do
-			local trig = existing[idx].trigger
-			if trig == "mk" or trig == "dm" then
-				table.remove(existing, idx)
-			end
-		end
-	end,
-})
-
 local function not_math()
 	return vim.fn["vimtex#syntax#in_mathzone"]() == 0
 end
 
+local function never_show()
+	return false
+end
+
 ls.add_snippets("tex", {
-	s({ trig = "mk", snippetType = "autosnippet", condition = not_math }, {
+	s({ trig = "mk", snippetType = "autosnippet", condition = not_math, priority = 10000 }, {
 		t("\\("),
 		i(1),
 		t("\\)"),
 	}),
-	s({ trig = "dm", snippetType = "autosnippet", condition = not_math }, {
+	s({ trig = "dm", snippetType = "autosnippet", condition = not_math, priority = 10000 }, {
 		t({ "\\[", "\t" }),
 		i(1),
 		t({ "", "\\]" }),
 		i(0),
 	}),
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	group = "Autocmds",
+	pattern = "tex",
+	callback = function()
+		local existing = ls.get_snippets("tex")
+		if not existing then
+			return
+		end
+		for _, snip in ipairs(existing) do
+			if (snip.trigger == "mk" or snip.trigger == "dm") and snip.priority ~= 10000 then
+				snip.show_condition = never_show
+				snip.hidden = true
+			end
+		end
+	end,
 })
